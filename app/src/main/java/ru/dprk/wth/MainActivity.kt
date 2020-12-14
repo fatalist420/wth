7579a78bda277f541b63695d0e0d927256e97eae
Fatalist420 <fatalist420@gmail.com>
2020-12-14 23:05:56 +0300

    не добавлена проверка номера телефона среди списка вызовов

--- app/src/main/java/ru/dprk/wth/MainActivity.kt
+++ app/src/main/java/ru/dprk/wth/MainActivity.kt
@@ -18,19 +18,25 @@ class MainActivity : AppCompatActivity() {
     var REQUEST_CODE_PERMISSION_READ_CONTACTS = 0
     var number: String = ""
     var durat: Long = -1
-    var type : Int = -1
+    var type: Int = -1
+    var date: Long = -1
     val list = ArrayList<newclass>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val permissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
+        val permissionStatus =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALL_LOG)
 
         if (permissionStatus == PackageManager.PERMISSION_GRANTED) {
             xtr()
         } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CALL_LOG), REQUEST_CODE_PERMISSION_READ_CONTACTS)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.READ_CALL_LOG),
+                REQUEST_CODE_PERMISSION_READ_CONTACTS
+            )
         }
 
 
@@ -40,11 +46,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
         when (requestCode) {
             REQUEST_CODE_PERMISSION_READ_CONTACTS -> {
                 if (grantResults.isNotEmpty()
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
                     Toast.makeText(this, "GRANTED", Toast.LENGTH_SHORT).show()
                     xtr()
                 } else {
@@ -58,22 +69,50 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     fun xtr() {
+        list.clear()
         val allCalls: Uri = Uri.parse("content://call_log/calls")
         val vHello = findViewById<TextView>(R.id.hello)
 
         val cursor = contentResolver.query(allCalls, null, null, null, null)
-        if (cursor!!.moveToLast()){
-            //var column_index = cursor.getColumnIndex(CallLog.Calls.NUMBER)
+
+        (cursor!!.moveToLast())
+        type = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE))
+        if (type == CallLog.Calls.OUTGOING_TYPE) {
             number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER))
             durat = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DURATION))
-            type = cursor.getInt(cursor.getColumnIndex(CallLog.Calls.TYPE))
-            if (type == 3) {
-                list.add(newclass(number, durat, type))
+            date = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DATE))
+            list.add(newclass(number, durat, type, date))
+        }
+
+        for (i in 0..50) {
+            if (cursor.moveToPrevious()) {
+                if (type == CallLog.Calls.OUTGOING_TYPE) {
+                    number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER))
+                    durat = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DURATION))
+                    date = cursor.getLong(cursor.getColumnIndex(CallLog.Calls.DATE))
+                    list.add(newclass(number, durat, type, date))
+                }
             }
         }
         cursor.close()
 
-        vHello.text = "$number $durat $type"
+        vHello.text = "${list[26]}"
+    }
+
+    // Обработка двойного нажатия кнопки "BACK" для выхода из приложения
+    private var backPress: Long = 0
+
+    override fun onBackPressed() {
+        if (backPress + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed()
+        } else {
+            backPress = System.currentTimeMillis()
+            Toast.makeText(
+                baseContext,
+                " Для выхода нажмите кнопку Назад еще раз",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
 
--- app/src/main/java/ru/dprk/wth/new class.kt
+++ app/src/main/java/ru/dprk/wth/new class.kt
@@ -1,7 +1,8 @@
 package ru.dprk.wth
 
-data class newclass (
-    var tel_number:String,
-    var tel_duration:Long,
-    var tel_type:Int
+data class newclass(
+    var tel_number: String,
+    var tel_duration: Long,
+    var tel_type: Int,
+    var tel_date: Long
 )
